13e7f5c578185f5fec482dca42c9b6ebd533728f
strogiyotec <almas337519@gmail.com>
2020-10-27 12:24:43 -0700

    implement avatar storage in weedfs

--- src/main/java/com/jwt/jwitter/models/mappers/UserMapper.java
+++ src/main/java/com/jwt/jwitter/models/mappers/UserMapper.java
@@ -1,14 +1,22 @@
 package com.jwt.jwitter.models.mappers;
 
+import com.jwt.jwitter.avatars.AvatarUrlProvider;
 import com.jwt.jwitter.models.User;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Service;
 
 /**
  * Db mapper for user.
  */
+@Service
 public final class UserMapper implements RowMapper<User> {
+
+    @Autowired
+    private AvatarUrlProvider avatarUrlProvider;
+
     @Override
     public User mapRow(final ResultSet rs, final int rowNum) throws SQLException {
         return new User(
@@ -17,7 +25,7 @@ public final class UserMapper implements RowMapper<User> {
             rs.getString("password"),
             rs.getString("username"),
             rs.getDate("birthday"),
-            null
+            this.avatarUrlProvider.normalizeUrl(rs.getString("avatar"))
         );
     }
 }
