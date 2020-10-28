ade102b6e3d24815a5672a79c423ea8599d76b12
jihee <developer.jiheuilee@gmail.com>
2020-10-28 01:27:26 -0700

    Added edit profile dialog frontend part

---
+++ frontend/src/components/middle/InputWithCounter.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Input from "@material-ui/core/Input";
+
+function InputWithCounter({name, inputType, placeHolder, maxChar, multiline, rows}) {
+    const [ numChar, setNumChar ] = React.useState("0");
+
+    function countChar(event){
+        console.log(event)
+        setNumChar(event.target.value.length);
+    }
+
+    return (
+        <div>
+        <Input name="name"
+               type={inputType}
+               placeholder={placeHolder}
+               maxLength={maxChar}
+               fullWidth
+               className="edit-profile-input"
+               onChange={countChar}
+               multiline={multiline}
+               rows={rows}
+        />
+        <span class="charcount-indicator">
+           {numChar}/{maxChar}
+        </span>
+        </div>
+    )
+}
+
+export default InputWithCounter
--- frontend/src/components/middle/Profile.js
+++ frontend/src/components/middle/Profile.js
@@ -1,16 +1,32 @@
 import React from 'react'
-import Tweet from '../middle/Tweet'
-import Post from '../middle/Post'
 import ProfileTabs from "./ProfileTabs";
 
 import axios from 'axios';
 import BackIcon from '@material-ui/icons/ArrowBack';
 import CalendarIcon from '@material-ui/icons/DateRange';
 import BdayIcon from '@material-ui/icons/CakeOutlined';
+import CloseIcon from '@material-ui/icons/Close';
+import AddAPhotoIcon from '@material-ui/icons/AddAPhotoOutlined';
 
 import {Button} from "@material-ui/core";
+import * as PropTypes from "prop-types";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import Input from '@material-ui/core/Input';
+import InputWithCounter from "./InputWithCounter";
 
 function Profile() {
+    const [editProfileOpen, setEditProfileOpen] = React.useState(true);
+
+    function handleClickOpen() {
+        setEditProfileOpen(true);
+    }
+
+    const handleClose = (value) => {
+        setEditProfileOpen(false);
+    };
+
     return (
         <div className="profile">
             <div className="profile_header">
@@ -21,7 +37,7 @@ function Profile() {
             <div className ="profileSubheader">
                 <div className="cover-photo"></div>
                 <div className="profile-image"></div>
-                <Button className="edit_btn">Edit Profile</Button>
+                <Button className="edit_btn" onClick={handleClickOpen}>Edit Profile</Button>
             </div>
             <div className="profileInfo">
                 <div className ="userName">userName</div>
@@ -49,8 +65,92 @@ function Profile() {
                 </div>
             </div>
             <ProfileTabs />
+            <EditProfileDialogue open={editProfileOpen} onClose={handleClose} />
         </div>
     )
+}
+EditProfileDialogue.propTypes = {
+    onClose: PropTypes.func.isRequired,
+    open: PropTypes.bool.isRequired,
+};
+
+function EditProfileDialogue(props) {
+    const { onClose, open } = props;
+
+    function handleClose() {
+        onClose();
+    };
+
+     function handleSaveProfile() {
+         // todo: CRUD UPDATE user here
+         handleClose();
+     }
+
+    function handleChangeCoverPhoto() {
+
+    }
+
+    function handleChangeProfilePhoto() {
+
+    }
+
+    return (
+        <Dialog onClose={handleClose} className="edit-profile-dialog" aria-labelledby="edit-profile-dialog" open={open}>
+            <DialogTitle id="edit-profile-dialog-header">
+                <CloseIcon className="close-profile-dialog" onClick={handleClose}/>
+                <span className="edit-profile-title">Edit Profile</span>
+                <Button className="save-profile-button" onClick={handleSaveProfile}>Save</Button>
+            </DialogTitle>
+            <DialogContent id="edit-profile-dialog-content">
+                <div className ="profileSubheader">
+                    <div className="cover-photo">
+                        <AddAPhotoIcon className="edit-photo-button" onClick={handleChangeCoverPhoto}/>
+                    </div>
+                    <div className="profile-image">
+                        <AddAPhotoIcon className="edit-photo-button" onClick={handleChangeProfilePhoto}/>
+                    </div>
+                </div>
+                <form action="/" method="" id="editUserForm">
+                    <div className="input-form">
+                        <label for="username">Name</label>
+                        <InputWithCounter numChar="20"
+                                          inputType="text"
+                                          placeHolder="Add your name"
+                                          maxChar="50"/>
+                    </div>
+                    <div className="input-form">
+                        <label for="bio">Bio</label>
+                        <InputWithCounter numChar="160"
+                                          inputType="text"
+                                          placeHolder="Add your bio"
+                                          maxChar="160"
+                                          multiline="true"
+                                          rows="3"/>
+                    </div>
+                    <div className="input-form">
+                        <label for="location">Location</label>
+                        <InputWithCounter numChar="0"
+                                          inputType="text"
+                                          placeHolder="Add your location"
+                                          maxChar="30"/>
+                    </div>
+                    <div className="input-form">
+                        <label for="website">Website</label>
+                        <InputWithCounter numChar="0"
+                                          inputType="text"
+                                          placeHolder="Add your website"
+                                          maxChar="100"/>
+                    </div>
+                    <div className="input-form">
+                        <label for="birthdate">Birth Date</label>
+                        <Input name="name"
+                               type="date"
+                               fullWidth/>
+                    </div>
+                </form>
+            </DialogContent>
+        </Dialog>
+    );
 }
 
 export default Profile
