9713b1fe3f587d96e270dd305718398c8000326d
jihee <developer.jiheuilee@gmail.com>
2020-12-02 14:41:46 -0800

    selenium test for deleting post

---
+++ src/test/java/com/jwt/jwitter/selenium/TestDeletePost.java
@@ -0,0 +1,47 @@
+package com.jwt.jwitter.selenium;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
+import java.util.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+public class TestDeletePost {
+    private WebDriver driver;
+    private Map<String, Object> vars;
+    JavascriptExecutor js;
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        js = (JavascriptExecutor) driver;
+        vars = new HashMap<String, Object>();
+    }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+    @Test
+    public void testDeletePost() {
+        driver.get("http://localhost:3000/home");
+        driver.manage().window().setSize(new Dimension(1080, 1920));
+        driver.findElement(By.cssSelector(".post:nth-child(1) .MuiButtonBase-root .MuiSvgIcon-root")).click();
+        driver.findElement(By.cssSelector(".MuiPaper-root:nth-child(3) .MuiSvgIcon-root")).click();
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/a[1]/div")).click();
+    }
+}
